047b89459d2c0d3b82ded4482f109949e634496a
Abhishek Mazumder <abhishekmazumder69@gmail.com>
2024-08-23 14:22:21 +0530

    Add: TicketDetails screen

---
+++ app/tickets/[id]/TicketDetails.tsx
@@ -0,0 +1,60 @@
+import { Ticket } from "@prisma/client";
+import React from "react";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import TicketStatusBadge from "@/components/TicketStatusBadge";
+import TicketPriority from "@/components/TicketPriority";
+import Link from "next/link";
+import { buttonVariants } from "@/components/ui/button";
+import ReactMarkdown from "react-markdown";
+
+interface Props {
+  ticket: Ticket;
+}
+
+const TicketDetails = ({ ticket }: Props) => {
+  return (
+    <div className="lg:grid lg:grid-cols-4">
+      <Card className="mx-4 mb-3 lg:col-span-3 lg:mr-3">
+        <CardHeader>
+          <div className="flex justify-between mb-3">
+            <TicketStatusBadge status={ticket.status} />
+            <TicketPriority priority={ticket.priority} />
+          </div>
+          <CardTitle>{ticket.title}</CardTitle>
+          <CardDescription>
+            Created: {ticket.createdAt.toLocaleDateString()}
+          </CardDescription>
+        </CardHeader>
+        <CardContent className="prose dark:prose-invert">
+          <ReactMarkdown>{ticket.description}</ReactMarkdown>
+        </CardContent>
+        <CardFooter>
+          <p>Updated: {ticket.updatedAt.toLocaleDateString()}</p>
+        </CardFooter>
+      </Card>
+      <div className="mx-4 flex lg:flex-col gap-2">
+        <Link
+          href={`/tickets/edit/${ticket.id}`}
+          className={`${buttonVariants({ variant: "default" })}`}
+        >
+          Edit Ticket
+        </Link>
+        <Link
+          href={`/tickets/edit/${ticket.id}`}
+          className={`${buttonVariants({ variant: "default" })}`}
+        >
+          Delete Ticket
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default TicketDetails;
---
+++ app/tickets/[id]/page.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import prisma from '@/prisma/db'
+import TicketDetails from './TicketDetails'
+
+interface Props{
+  params: {id : string}
+}
+
+const ViewTicket = async({params}: Props) => {
+  const ticket = await prisma.ticket.findUnique({
+    where:{id: parseInt(params.id)}
+  })
+
+  if(!ticket){
+    return <p className='text-destructive'>No Ticket Found!</p>
+  }
+  return (
+    <div>
+      <TicketDetails ticket={ticket} />
+    </div>
+  )
+}
+
+export default ViewTicket
