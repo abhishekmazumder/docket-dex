097d553b830baa8ed5ab5d01d263607efee66888
Abhishek Mazumder <abhishekmazumder69@gmail.com>
2024-08-24 13:19:26 +0530

    Add: Pagination funtionality

---
+++ components/Pagination.tsx
@@ -0,0 +1,61 @@
+"use client"
+
+import React from "react";
+import { Button } from "./ui/button";
+import {
+  ChevronFirst,
+  ChevronLast,
+  ChevronLeft,
+  ChevronRight,
+} from "lucide-react";
+import { useRouter, useSearchParams } from "next/navigation";
+
+interface Props {
+  itemCount: number;
+  pageSize: number;
+  currentPage: number;
+}
+
+const Pagination = ({ itemCount, pageSize, currentPage }: Props) => {
+  const pagesCount = Math.ceil(itemCount / pageSize);
+  const router = useRouter();
+  const searchParams = useSearchParams();
+
+  if (pagesCount <= 1) return null;
+
+  // const changePage = (page) =>{
+  //   // const newPage = searchParams.get('page') ? parseInt(searchParams.get('page')!)
+  // }
+
+  const changePage = (page: number) => {
+    const params = new URLSearchParams(searchParams);
+    params.set("page", page.toString());
+    router.push("?" + params.toString());
+  };
+
+  return (
+    <div className="mt-10 mb-6">
+      <div>
+        <Button variant="outline" disabled={currentPage === 1} onClick={() => changePage(1)}>
+          <ChevronFirst />
+        </Button>
+        <Button variant="outline" disabled={currentPage === 1} onClick={() => changePage(currentPage - 1)}>
+          <ChevronLeft />
+        </Button>
+        <Button variant="outline" disabled={currentPage === pagesCount} onClick={() => changePage(currentPage + 1)}>
+          <ChevronRight />
+        </Button>
+        <Button variant="outline" disabled={currentPage === pagesCount} onClick={() => changePage(pagesCount)}>
+          <ChevronLast />
+        </Button>
+      </div>
+      <div>
+        <p>
+          Page {currentPage} of {pagesCount}
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Pagination;
